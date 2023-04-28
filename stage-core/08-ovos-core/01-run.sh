4b1f41e80d1ac87024c81b2acb6246d06f5af11d
builderjer <builderjer@gmail.com>
2023-04-27 19:26:27 -0600

    rename stages

--- stage-core/08-ovos-core/01-run.sh
+++ stage-core/08-ovos-core/01-run.sh
@@ -7,8 +7,6 @@ install -v -m 0755 files/ovos-systemd-messagebus "${ROOTFS_DIR}/usr/libexec/ovos
 install -v -m 0644 files/ovos.service "${ROOTFS_DIR}/etc/systemd/user/ovos.service"
 install -v -m 0644 files/ovos-messagebus.service "${ROOTFS_DIR}/etc/systemd/user/ovos-messagebus.service"
 
-install -v -d -m 0755 "${ROOTFS_DIR}/etc/mycroft"
-
 on_chroot << EOF
 
 pip3 install sdnotify
